17ab7c0e6bc114b85c79348683ec03cfa2adc729
Emilios <emilioschristou1@gmail.com>
2020-05-11 20:02:13 +0100

    qpsk modulation

--- audio_modem/network_stack/link_layer/transmit/modulate/modulator.py
+++ audio_modem/network_stack/link_layer/transmit/modulate/modulator.py
@@ -14,20 +14,26 @@ def integer_to_bitarray(ints, size):
     
     return np.fromiter(it.chain.from_iterable(binary_words), dtype=np.int8)
 
+def bytes_to_bits(data):
+    #To Do
+    return True
+
 
 data = np.array([0,0,1,1,0,1,1,0,0,1,1,0]) #example data for testing
 
 #3) Constellation Map
 
-const_map = {
+
+
+    
+def modulation(bits,const_length=2):
+    
+    const_map = {
     (0,0): 1+1j,
     (0,1): -1+1j,
     (1,1): -1-1j,
     (1,0): 1-1j,
 }
-
-    
-def modulation(bits,const_length=2):
 
     #First Test that the constellation length for modulation is valid
     const_check = np.log2(const_length)
@@ -35,13 +41,11 @@ def modulation(bits,const_length=2):
         raise ValueError('Constellation length must be a power of 2.')
     
     #Divide into blocks equal to constellation length
-    split_data = [bits[i * const_length:(i + 1) * const_length] for i in range((len(bits) + const_length - 1) // const_length )]  
-   
+    split_data = [tuple(bits[i * const_length:(i + 1) * const_length]) for i in range((len(bits) + const_length - 1) // const_length )]  
+    
+    #split_data = [tuple(i) for i in split_data]
+    
     #Map the blocks of data into constellations
     mapped_data = [const_map[values] for values in split_data]
        
-    ## Help, when I try to iterate over the numpy arrays it shows "TypeError: unhashable type: 'numpy.ndarray'"
-    
     return mapped_data
-
-print(modulation(data))   
