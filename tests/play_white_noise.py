3c60f267db95f7565d09b15e4fefcb1542e348f7
Tom Crossley FEARNLEY <tcac2@cam.ac.uk>
2020-05-11 17:22:03 +0100

    edit white noise

--- tests/play_white_noise.py
+++ tests/play_white_noise.py
@@ -5,7 +5,6 @@ A simple script to play a white noise
 import numpy as np
 import sounddevice as sd
 
-FREQUENCY = 440
 SAMPLE_RATE = 44100
 DURATION = 5
 
@@ -13,15 +12,11 @@ samples = SAMPLE_RATE * DURATION
 
 time_array = np.linspace(0, DURATION, samples, False)
 
-pure_signal = np.sin(FREQUENCY * time_array * 2 * np.pi)
-
 noise_signal = np.random.normal(0, 1, pure_signal.shape)
 
-total_signal = pure_signal + noise_signal
+noise_signal *= 32767 / np.max(np.abs(noise_signal))
 
-total_signal *= 32767 / np.max(np.abs(total_signal))
+noise_signal = noise_signal.astype(np.int16)
 
-total_signal = total_signal.astype(np.int16)
-
-sd.play(total_signal, SAMPLE_RATE)
+sd.play(noise_signal, SAMPLE_RATE)
 sd.wait()
